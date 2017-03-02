95b2e634cf68326b4a3852028ef4dd662102c8cc
cjw1115 <cjw1115@CQ-SURFACEBOOK>
2017-03-02 22:41:18 +0800

    添加项目文件。

---
+++ CQ.Controls.CarouselView/CQ.Controls.CarouselView.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CQ.Controls
+{
+    public class MyCarouselView : CarouselView
+    {
+
+        //public static readonly BindableProperty PeriodProperty = BindableProperty.Create("Period", typeof(double), typeof(MyCarouselView), 3000);
+        //public double Period
+        //{
+        //    get { return (double)GetValue(PeriodProperty); }
+        //    set { SetValue(PeriodProperty, value); }
+        //}
+
+        #region CarouselView轮播
+
+        public static readonly BindableProperty AutoPlayProperty = BindableProperty.Create("AutoPlay", typeof(bool?), typeof(MyCarouselView), null, propertyChanged: AutoPlayBindingPropertyChanged);
+        public bool? AutoPlay
+        {
+            get { return (bool?)this.GetValue(AutoPlayProperty); }
+            set
+            {
+                SetValue(AutoPlayProperty, value);
+            }
+        }
+
+        private static int _itemsCount = 0;
+        private static CarouselView _cv;
+        public static void AutoPlayBindingPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if ((bool)newValue == true)
+            {
+                CarouselView cv = bindable as CarouselView;
+                _cv = cv;
+                //var period = (double?)cv.GetValue(PeriodProperty);
+                //if (period == null)
+                //{
+                //    period = 2000;
+                //}
+                double period = 3000;
+                Device.StartTimer(System.TimeSpan.FromMilliseconds(period), () =>
+                {
+                    IList list = _cv.ItemsSource as IList;
+                    _itemsCount = list.Count;
+                    if (_itemsCount == 0)
+                        return false;
+                    _cv.Position = (_cv.Position + 1) % _itemsCount;
+
+                    var re = (bool)_cv.GetValue(AutoPlayProperty);
+                    return re;
+                });
+            }
+        }
+
+        #endregion
+    }
+}
